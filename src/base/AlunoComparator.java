d040ec894f9d7e61d13b40d6b1b8f36cf225678a
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-02-22 13:23:50 -0300

    Comparator documentado

--- src/base/AlunoComparator.java
+++ src/base/AlunoComparator.java
@@ -13,6 +13,7 @@ public class AlunoComparator implements Comparator<Aluno> {
 	 * Compara os nomes de dois alunos e retorna a diferença entre eles
 	 * @param aluno1 um objeto Aluno
 	 * @param aluno2 outro objeto Aluno
+	 * @return inteiro com a diferença das strings de nome 
 	 */
 	@Override
 	public int compare(Aluno aluno1, Aluno aluno2) {
