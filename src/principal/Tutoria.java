048bac8ec363b17f36386fe7f61b1963c3254a7b
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-02-23 09:05:55 -0300

    Metodo de validar disciplina e proficiencia

--- src/principal/Tutoria.java
+++ src/principal/Tutoria.java
@@ -6,27 +6,29 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Essa classe representa um aluno tutor, que possui, alem de suas informacoes como aluno, 
- * a disciplina que ele ensina, a proficiencia em relacao a essa disciplina, uma lista de 
- * locais que atende e a lista de dias e horarios disponiveis.
+ * Essa classe representa um aluno tutor, que possui, alem de suas informacoes
+ * como aluno, a disciplina que ele ensina, a proficiencia em relacao a essa
+ * disciplina, uma lista de locais que atende e a lista de dias e horarios
+ * disponiveis.
  *
  */
 public class Tutoria {
 
 	private String disciplina;
-	private int proficiencia, avaliacao;
+	private int proficiencia;
+	private int avaliacao;
 	private List<String> locais;
 	private Map<String, List<String>> horarios;
-	
+
 	public Tutoria(String disciplina, int proficiencia) {
 		validar(disciplina, proficiencia);
-		
+
 		this.disciplina = disciplina;
 		this.proficiencia = proficiencia;
 		this.locais = new ArrayList<>();
 		this.horarios = new HashMap<>();
 	}
-	
+
 	public String getDisciplina() {
 		return disciplina;
 	}
@@ -40,6 +42,11 @@ public class Tutoria {
 	}
 
 	private void validar(String disciplina, int proficiencia) {
-		
+		if (disciplina == null || disciplina.trim().equals("")) {
+			throw new IllegalArgumentException("Erro na definicao de papel: Disciplina nao pode ser nula ou vazia");
+		}
+		if (proficiencia <= 0) {
+			throw new IllegalArgumentException("Erro na definicao de papel: Proficiencia invalida");
+		}
 	}
 }
