a3d544e41e1a41445d3aa4a7e91daf7de9998d0e
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-02-23 14:48:53 -0300

    ListarTutores implementado

--- src/principal/AlunoController.java
+++ src/principal/AlunoController.java
@@ -69,15 +69,24 @@ public class AlunoController {
 	public String listarAlunos() {
 		Collections.sort(alunos, comparator);
 		String lista = "";
-		for (int i = 0; i < alunos.size(); i++) {
-			lista += alunos.get(i).toString();
-			if (i != alunos.size() - 1) {
+		for (Aluno aluno : alunos) {
+			if (!lista.equals("")) {
 				lista += ", ";
 			}
+			lista += aluno.toString();
 		}
 		return lista;
 	}
 
+	/**
+	 * Retorna a informacao especifica do aluno
+	 * 
+	 * @param matricula
+	 *            matricula do aluno
+	 * @param atributo
+	 *            categoria do dado desejado
+	 * @return um dado do aluno
+	 */
 	public String getInfoAluno(String matricula, String atributo) {
 		Aluno aluno = procuraAluno(matricula);
 		if (aluno == null) {
@@ -86,6 +95,16 @@ public class AlunoController {
 		return aluno.getInfo(atributo);
 	}
 
+	/**
+	 * Transforma um aluno em tutor da disciplina informada
+	 * 
+	 * @param matricula
+	 *            matricula do novo tutor
+	 * @param disciplina
+	 *            disciplina a ser ensinada
+	 * @param proficiencia
+	 *            nivel de proficiencia para essa disciplina
+	 */
 	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
 		Aluno aluno = procuraAluno(matricula);
 		if (aluno == null) {
@@ -94,6 +113,13 @@ public class AlunoController {
 		aluno.tornarTutor(disciplina, proficiencia);
 	}
 
+	/**
+	 * Retorna as informacoes de um aluno tutor atraves de sua matricula
+	 * 
+	 * @param matricula
+	 *            matricula do aluno
+	 * @return dados do tutor
+	 */
 	public String recuperaTutor(String matricula) {
 		Aluno aluno = procuraAluno(matricula);
 		if (aluno == null) {
@@ -102,8 +128,23 @@ public class AlunoController {
 		return aluno.toString();
 	}
 
+	/**
+	 * Lista todos os alunos que sao tutores
+	 * 
+	 * @return lista com os dados dos tutores
+	 */
 	public String listarTutores() {
-		return "";
+		String lista = "";
+		for (Aluno aluno : alunos) {
+			if (aluno.ehTutor()) {
+				if (!lista.equals("")) {
+					lista += ", ";
+				}
+				lista += aluno.toString();
+			}
+		}
+
+		return lista;
 	}
 
 	public void cadastrarHorario(String email, String horario, String dia) {
