b68b481e9eec7407298e9e0b53abb8ecd919de8a
allemoura <alessandra.moura@ccc.ufcg.edu.br>
2018-02-26 18:25:09 -0300

    tests AlunoController atualizados

--- test/principal/AlunoControllerTest.java
+++ test/principal/AlunoControllerTest.java
@@ -119,4 +119,88 @@ public class AlunoControllerTest {
 	public void getInfoAlunoNaoCadastradoTest() {
 		this.alunoController.getInfoAluno("234523", "Nome");
 	}
+	/**
+	 * Espera-se que torne o Aluno um tutor
+	 */
+	@Test
+	public void tornarTutorTest() {
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+	}
+	/**
+	 * Espera-se que lance uma excecao, pois o aluno nao foi cadastrado
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void tornarTutorAlunoNaoCadastradoTest() {
+		this.alunoController.tornarTutor("237890", "Calculo I", 3);
+	}
+	/**
+	 * Espera-se que recupere o Tutor que foi cadastrado
+	 */
+	@Test
+	public void recuperaTutorTest() {
+		Aluno novoAluno = new Aluno("Joao", "1232423", "DD34", "8467989023", "joaozinho@blizzard.com");
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+		assertEquals(novoAluno.toString(), this.alunoController.recuperaTutor("1232423"));
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void recuperarTutorAlunoNaoCadastradoTest() {
+		this.alunoController.recuperaAluno("232342");
+	}
+	@Test
+	public void listarTutores() {
+		this.alunoController.cadastrarAluno("Eliza", "345322", "CC44", "84679452353", "elizinha@blizzard.com");
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+		this.alunoController.tornarTutor("345322", "Calculo I", 3);
+		assertEquals("1232423 - Joao - DD34 - 8467989023 - joaozinho@blizzard.com, 345322 - Eliza - CC44 - 84679452353 - elizinha@blizzard.com",this.alunoController.listarTutores());
+	}
+	@Test
+	public void cadastrarHorarioTest() {
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+		this.alunoController.cadastrarHorario("joaozinho@blizzard.com", "17:30", "21/03");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void cadastrarHorarioEmailNuloTest() {
+		this.alunoController.cadastrarHorario(null, "17:30", "21/03");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void cadastrarHorarioEmailVazioTest() {
+		this.alunoController.cadastrarHorario("", "17:30", "21/03");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void cadastrarHorarioTutorNCadastradoTest() {
+		this.alunoController.cadastrarHorario("elizinha@blizzard.com", "14:30", "23/03");
+	}
+	@Test
+	public void cadastrarLocalDeAtendimentoTest() {
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+		this.alunoController.cadastrarLocalDeAtendimento("joaozinho@blizzard.com", "LCC3");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void cadastrarLocalDeAtendimentoNuloTest() {
+		this.alunoController.cadastrarLocalDeAtendimento(null, "LCC3");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void cadastrarLocalDeAtendimentoVazioTest() {
+		this.alunoController.cadastrarLocalDeAtendimento("", "LCC3");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void cadastrarLocalDeAtendimentoTutorNCadastradoTest() {
+		this.alunoController.cadastrarLocalDeAtendimento("elizinha@blizzard.com", "LCC3");
+	}
+	@Test
+	public void consultaHorarioTest() {
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+		this.alunoController.cadastrarHorario("joaozinho@blizzard.com", "17:30", "21/03");
+		assertEquals(true,this.alunoController.consultaHorario("joaozinho@blizzard.com", "17:30", "21/03"));
+	}
+	@Test
+	public void consultaHorarioNCadastradoTest() {
+		this.alunoController.tornarTutor("1232423", "Programacao", 4);
+		this.alunoController.cadastrarHorario("joaozinho@blizzard.com", "14:30", "21/03");
+		assertEquals(false,this.alunoController.consultaHorario("joaozinho@blizzard.com", "17:30", "21/03"));
+	}
+	@Test
+	public void consultaHorarioTutorNCadastradoTest() {
+		assertEquals(false,this.alunoController.consultaHorario("elizinha@blizzard.com", "17:30", "21/03"));
+	}
 }
