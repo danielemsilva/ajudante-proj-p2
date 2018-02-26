468e3a873e833df10cf02d2523166ecf027207c9
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-02-25 21:38:33 -0300

    Atualização de us2 e us3 e correção de código para o us3

--- src/principal/Facade.java
+++ src/principal/Facade.java
@@ -51,8 +51,8 @@ public class Facade {
 	}
 
 	public static void main(String[] args) {
-		args = new String[] { "principal.Facade", "acceptance_test/us1_test.txt",
-				"acceptance_test/us2_test.txt", "acceptance_test/us3_test.txt"};
+		args = new String[] { "principal.Facade", "acceptance_test/us1_test.txt", "acceptance_test/us2_test.txt",
+				"acceptance_test/us3_test.txt" };
 		EasyAccept.main(args);
 	}
 }
